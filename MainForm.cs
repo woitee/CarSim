dffeb53a42cfc651e93c67ced37083d452bb6772
woitee <woitee@gmail.com>
2013-07-05 22:05:21 +0200

    Few basic classes

--- MainForm.cs
+++ MainForm.cs
@@ -5,24 +5,20 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-//using System.Threading.Tasks;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CarSim
 {
     public partial class MainForm : Form
     {
+        Simulation sim = new Simulation();
         public MainForm()
         {
             InitializeComponent();
-            Bitmap bmp = new Bitmap(640,480);
-            pictureBox.Image=bmp;
-            for (int i = 0; i < 20; i++){
-                for (int j = 0; j < 15; j++){
-                    Graphics g = Graphics.FromImage(bmp);
-                    g.DrawImage(Properties.Resources.Road,i*32,j*32);
-                }
-            }
+            pictureBox.Image = sim.DrawBackground();
+            sim.Load();
+            sim.Save("save2.txt");
         }
     }
 }
---
+++ Simulation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+
+namespace CarSim
+{
+    class Simulation
+    {
+        private Car[] cars;
+        private char[,] arr = new char[20,15];
+
+        public Bitmap DrawBackground(){
+            Bitmap bmp = new Bitmap(640,480);
+            for (int i = 0; i < 20; i++){
+                for (int j = 0; j < 15; j++){
+                    Graphics g = Graphics.FromImage(bmp);
+                    g.DrawImage(Properties.Resources.Road,i*32,j*32);
+                }
+            }
+            return bmp;
+        }
+
+        public void Load(string path = "save.txt"){
+            //save map
+            StreamReader sr = new StreamReader(path);
+            for (int i = 0; i < 15; i++){
+                string line = sr.ReadLine();
+                for (int j = 0; j < 20; j++){
+                    arr[j,i]=line[j];
+                }
+            }
+            sr.Close();
+        }
+
+        public void Save(string path = "save.txt"){
+            //save map
+            StreamWriter sw = new StreamWriter(path);
+            for (int i = 0; i < 15; i++){
+                StringBuilder sb = new StringBuilder("********************");
+                for (int j = 0; j < 20; j++){
+                    sb[j]=arr[j,i];
+                }
+                sw.WriteLine(sb.ToString());
+            }
+            sw.Close();
+        }
+    }
+}
